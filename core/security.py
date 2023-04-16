62e98497dbd37cf85244ccdb9a93392b62738a3c
Pavel <egorovpavel331@mail.ru>
2023-04-17 02:09:55 +0300

    fix

--- core/security.py
+++ core/security.py
@@ -15,6 +15,6 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
         )
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
-        to_encode, "super-secret-key", algorithm="HS256"
+        to_encode, "super-secret-keys", algorithm="HS256"
     )
     return encoded_jwt
